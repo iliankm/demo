0df200809f0c50ab0d9db3dc00d90a41a2a69541
Iliyan Kamilov <ilian.kamilov@gmail.com>
2021-10-12 11:47:58 +0300

    ResponseExceptionAdvice.java

---
+++ src/main/java/com/iliankm/demo/controller/ResponseExceptionAdvice.java
@@ -0,0 +1,27 @@
+package com.iliankm.demo.controller;
+
+import lombok.SneakyThrows;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ValidationException;
+
+/**
+ * Spring aware exception advice, that is applied after exception is thrown.
+ */
+@ControllerAdvice
+class ResponseExceptionAdvice extends ResponseEntityExceptionHandler {
+
+    /**
+     * Handles all javax validation exceptions.
+     *
+     * @param ex the {@link ValidationException}
+     */
+    @ExceptionHandler(ValidationException.class)
+    @SneakyThrows
+    public ResponseEntity<String> handleValidationException(ValidationException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
